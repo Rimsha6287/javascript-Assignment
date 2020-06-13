6383ebafb6f5d73e93b66c9eb6de7550eee334fe
Rimsha6287 <62468833+Rimsha6287@users.noreply.github.com>
2020-06-13 06:37:59 -0700

    fisrt website of javascript

---
+++ app.js
@@ -0,0 +1,38 @@
+
+//chap#1
+alert("Error! please enter a valid password.")
+alert("Welcome to JS Land... \n Happig Coding")
+alert("Welcome to JS Land... ")
+alert("Happy Coading! \n Prevent this page from creating additional dialogs")
+
+//chap#2
+
+var username
+var myname
+myname = "Rimsha Mobeen"
+
+//Q#3
+
+var Message
+Message = "Hello World"
+alert(Message);
+
+//Q#4
+
+alert (prompt("name"))
+alert (prompt("age"))
+alert (prompt("course"))
+
+
+//Q#5
+
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+//Q#6
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ")
+
+//chap#3
+
+
+
